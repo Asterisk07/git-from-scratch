412661d92a0939a6815bec265352247b1ef7577f
Asterisk07 <ferbandphineas100@gmail.com>
2026-04-05 11:46:39 +0530

    python : Add silent logging

--- python/utils.py
+++ python/utils.py
@@ -2,3 +2,25 @@ import sys
 
 def cat(x):
     sys.stdout.buffer.write(x)
+
+
+import logging
+from contextlib import contextmanager
+
+@contextmanager
+def silent():
+    """
+    Temporarily disables logging up to the specified level.
+    Defaults to DEBUG.
+    """
+    # Capture the current global disable level
+    previous_level = logging.root.manager.disable
+    
+    # Disable logging up to the requested level
+    logging.disable(logging.DEBUG)
+    
+    try:
+        yield
+    finally:
+        # Restore the original logging level
+        logging.disable(previous_level)
