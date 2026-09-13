0def1f62b7a86f25bf8d19e3bdceddfad97cbaef
Asterisk07 <ferbandphineas100@gmail.com>
2026-09-13 11:12:50 +0530

    Move repo logic into repo

--- rust/src/init.rs
+++ rust/src/init.rs
@@ -38,95 +38,95 @@ impl Repository {
             config: config,
         }
     }
-}
 
-fn repo_path(repo: Repository, path: &str) -> PathBuf {
-    // """Compute path under repo's gitdir."""
-    repo.gitdir.join(path)
-}
+    fn repo_path(&self, path: &str) -> PathBuf {
+        // """Compute path under repo's gitdir."""
+        self.gitdir.join(path)
+    }
 
-fn repo_file(repo: Repository, path: &str, mkdir: bool) -> PathBuf {
-    // """Compute path under repo's gitdir."""
-    repo.gitdir.join(path)
-}
+    fn repo_file(&self, path: &str, mkdir: bool) -> PathBuf {
+        // """Compute path under repo's gitdir."""
+        self.gitdir.join(path)
+    }
 
-fn repo_create(path: &str) -> Repository {
-    let repo = Repository::new(path, true);
-    let path = &repo.worktree;
+    fn repo_create(path: &str) -> Self {
+        let repo = Self::new(path, true);
+        let path = &repo.worktree;
 
-    // Ok(entries.next().is_none())
-    let gitpath = &repo.gitdir;
-    if path.exists() {
-        assert!(path.is_dir(), "Path is an existing file : {:?}", path);
-        if gitpath.exists() {
-            assert!(
-                gitpath.is_dir(),
-                "gitdir is an existing file : {:?}",
-                gitpath
-            );
-            assert!(
-                fs::read_dir(&gitpath)
-                    .expect("Error reading git dir")
-                    .next()
-                    .is_none(),
-                "Git dir non empty : {:?}",
-                gitpath
-            );
+        // Ok(entries.next().is_none())
+        let gitpath = &repo.gitdir;
+        if path.exists() {
+            assert!(path.is_dir(), "Path is an existing file : {:?}", path);
+            if gitpath.exists() {
+                assert!(
+                    gitpath.is_dir(),
+                    "gitdir is an existing file : {:?}",
+                    gitpath
+                );
+                assert!(
+                    fs::read_dir(&gitpath)
+                        .expect("Error reading git dir")
+                        .next()
+                        .is_none(),
+                    "Git dir non empty : {:?}",
+                    gitpath
+                );
+            }
+        } else {
+            fs::create_dir(&path).expect("Failed to create worktree");
+            fs::create_dir(&gitpath)
+                .expect(&format!("Failed to create git directory at {:?}", gitpath));
         }
-    } else {
-        fs::create_dir(&path).expect("Failed to create worktree");
-        fs::create_dir(&gitpath)
-            .expect(&format!("Failed to create git directory at {:?}", gitpath));
+
+        fs::create_dir(gitpath.join("branches")).unwrap();
+        fs::create_dir(gitpath.join("objects")).unwrap();
+        fs::create_dir(gitpath.join("refs")).unwrap();
+        fs::create_dir(gitpath.join("refs").join("tags")).unwrap();
+        fs::create_dir(gitpath.join("refs").join("heads")).unwrap();
+
+        // Python: open("file.txt", "w")
+
+        writeln!(
+            File::create(gitpath.join("HEAD")).unwrap(),
+            "ref : refs/heads/master"
+        )
+        .unwrap();
+        writeln!(
+            File::create(gitpath.join("description")).unwrap(),
+            "Unnamed repo, edit description to name it"
+        )
+        .unwrap();
+        // writeln!(
+        //     File::create(path.join("config")).unwrap(),
+        //     "ref : refs/heads/mastere"
+        // )
+        // .unwrap();
+        let con = Self::repo_default_config();
+        con.write(&repo.config_path).unwrap();
+        repo
     }
 
-    fs::create_dir(gitpath.join("branches")).unwrap();
-    fs::create_dir(gitpath.join("objects")).unwrap();
-    fs::create_dir(gitpath.join("refs")).unwrap();
-    fs::create_dir(gitpath.join("refs").join("tags")).unwrap();
-    fs::create_dir(gitpath.join("refs").join("heads")).unwrap();
-
-    // Python: open("file.txt", "w")
-
-    writeln!(
-        File::create(gitpath.join("HEAD")).unwrap(),
-        "ref : refs/heads/master"
-    )
-    .unwrap();
-    writeln!(
-        File::create(gitpath.join("description")).unwrap(),
-        "Unnamed repo, edit description to name it"
-    )
-    .unwrap();
-    // writeln!(
-    //     File::create(path.join("config")).unwrap(),
-    //     "ref : refs/heads/mastere"
-    // )
-    // .unwrap();
-    let con = repo_default_config();
-    con.write(&repo.config_path).unwrap();
-    repo
-}
-
-fn repo_default_config() -> Ini {
-    let mut con = Ini::new();
-    con.set("core", "repositoryformatversion", Some("0".to_string()));
-    con.set("core", "filemode", Some("false".to_string()));
-    con.set("core", "bare", Some("false".to_string()));
-    con
-}
-
-fn repo_find(path: PathBuf) {
-    // need canocical path
-    let x = path.join(".git");
-    if x.is_dir() {
-        return println!("found git dir at {:?}", x);
+    fn repo_default_config() -> Ini {
+        let mut con = Ini::new();
+        con.set("core", "repositoryformatversion", Some("0".to_string()));
+        con.set("core", "filemode", Some("false".to_string()));
+        con.set("core", "bare", Some("false".to_string()));
+        con
     }
-    // println!("Se at {:?}", path);
-    let path = path
-        .parent()
-        .expect(&format!("No git repo found at {:?}", path))
-        .to_path_buf();
-    return repo_find(path);
+
+    fn repo_find(path: PathBuf) {
+        // need canocical path
+        let x = path.join(".git");
+        if x.is_dir() {
+            return println!("found git dir at {:?}", x);
+        }
+        // println!("Se at {:?}", path);
+        let path = path
+            .parent()
+            .expect(&format!("No git repo found at {:?}", path))
+            .to_path_buf();
+        return Self::repo_find(path);
+    }
 }
 
 pub fn cmd_init(path: String) {
@@ -142,6 +142,6 @@ pub fn cmd_init(path: String) {
     //         .expect(&format!("Could not canocialise {:?}", path)),
     // )
     let path = &path;
-    repo_create(path);
+    Repository::repo_create(path);
     println!("created repo at");
 }
