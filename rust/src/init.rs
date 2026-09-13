b75faba7bac2445097fb441f8ac0e6f903c7a9f6
Asterisk07 <ferbandphineas100@gmail.com>
2026-09-13 11:20:18 +0530

    Add init cmd

---
+++ rust/src/init.rs
@@ -0,0 +1,18 @@
+use crate::models::repo::Repository;
+
+pub fn cmd_init(path: String) {
+    // let path = match args.len() {
+    //     2 => ".",      // git init
+    //     3 => &args[2], // git init dir
+    //     _ => panic!("error: too many arguments for `init`"),
+    // };
+    println!("creating repo at  {:?}", path);
+    // repo_find(
+    //     PathBuf::from(path)
+    //         .canonicalize()
+    //         .expect(&format!("Could not canocialise {:?}", path)),
+    // )
+    let path = &path;
+    Repository::repo_create(path);
+    println!("created repo at");
+}
